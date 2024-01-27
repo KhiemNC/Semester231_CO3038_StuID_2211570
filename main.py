3dd94d7855d73b4fe57bb6f4e0d92fe971ea739c
khiemnc <khiemnc04@gmail.com>
2024-01-28 00:08:02 +0700

    Add Adafruit_MQTT class definition and main file

---
+++ main.py
@@ -0,0 +1,43 @@
+# paho-mqtt library
+from adafruit_mqtt import Adafruit_MQTT
+import time
+import random
+
+# FUNCTION DEFINITIONS
+def callBackFunc_Message(feed_id, payload):
+    print("Feed: " + feed_id + " - Value: " + payload)
+    print("          or do something else...")
+
+# MAIN PROGRAM
+# Create an instance of Adafruit_MQTT class
+username = "khiemnc"
+key = "aio_gfRj207tWs0L1bTsap1nrWxyPKZ3"
+aio_feed_ids = ["button1", 	"button2"]
+khiem_client = Adafruit_MQTT(username, key, aio_feed_ids, callBackFunc_Message)
+khiem_client.setup()
+khiem_client.connect_and_loop()
+
+# Test publish data to Adafruit IO every 5 seconds
+counter_runtime = 10
+counter_loop = 5
+sensor_type = 1
+
+while counter_runtime > 0:
+    counter_runtime -= 1
+    counter_loop -= 1
+    
+    if counter_loop == 0:
+        counter_loop = 5
+        if sensor_type == 1:
+            value = random.randrange(-10, 50)
+            khiem_client.publish('sensor1', value)
+            sensor_type = 2
+        elif sensor_type == 2:
+            value = random.randint(0, 500)
+            khiem_client.publish('sensor2', value)
+            sensor_type = 3
+        elif sensor_type == 3:
+            value = random.randint(0, 100)
+            khiem_client.publish('sensor3', value)
+            sensor_type = 1
+    time.sleep(1)
