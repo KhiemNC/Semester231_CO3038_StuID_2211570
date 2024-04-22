5163a3efba25d9be8b64aeeaf8ce20aa741abc0c
khiem.nguyen <nguyencongkhiem510@gmail.com>
2024-04-22 08:45:36 +0700

    Implement MqttManager.java contain MqttManager class, use it to connect MQTT with Adafruit

--- AndroidStudioApp/app/src/main/java/com/example/dbiot/MainActivity.java
+++ AndroidStudioApp/app/src/main/java/com/example/dbiot/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.dbiot;
+// import com.example.dbiot.BuildConfig;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+// import org.eclipse.paho.android.service.BuildConfig;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
 public class MainActivity extends AppCompatActivity {
+
+    MqttManager myMqtt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +28,41 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        startMQTT();
+    }
+
+    public void startMQTT(){
+        String clientId = "2211570";
+        String username = BuildConfig.MQTT_USERNAME;
+        String password = BuildConfig.MQTT_PASSWORD;
+        String serverUri = "tcp://io.adafruit.com:1883";
+        String[] arrayTopics = {"khiemnc/feeds/sensor1",
+                                "khiemnc/feeds/sensor2",
+                                "khiemnc/feeds/sensor3",
+                                "khiemnc/feeds/button1",
+                                "khiemnc/feeds/button2"};
+        myMqtt = new MqttManager(this, clientId, username, password, serverUri, arrayTopics);
+        myMqtt.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean reconnect, String serverURI) {
+
+            }
+
+            @Override
+            public void connectionLost(Throwable cause) {
+
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                Log.d("TEST", topic + "->" + message.toString());
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+
+            }
         });
     }
 }
